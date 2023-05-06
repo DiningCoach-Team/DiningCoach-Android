e4f37403d1c77c3ba6503926c4612685d107b9de
tunameatball <kh9768@gmail.com>
2023-05-06 10:44:49 +0900

    [2023.05.06.김광희] 1. Retrofit 기본 클래스 생성

---
+++ app/src/main/java/com/dining/coach/util/RetrofitClient.kt
@@ -0,0 +1,37 @@
+package com.dining.coach.util
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+
+    companion object {
+        private const val BASE_URL = "https://base.server.dev/"
+
+        val INSTANCE: Retrofit by lazy {
+            getInstance()
+        }
+
+        private fun getInstance(): Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(createClient())
+            .build()
+
+
+        private fun createClient(): OkHttpClient {
+            val clientBuilder = OkHttpClient.Builder()
+
+            clientBuilder.addInterceptor(
+                HttpLoggingInterceptor().setLevel(
+                    HttpLoggingInterceptor.Level.BODY
+                )
+            )
+
+            return clientBuilder.build()
+        }
+    }
+}
+
