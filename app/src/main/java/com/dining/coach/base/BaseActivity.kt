e9a655f08cc13a6cdc06c062d62e295db7576ae7
master <82108@DESKTOP-SDBJJB4>
2023-05-10 01:29:30 +0900

    [2023.05.10.강범석] 1. LayoutManager in BaseActivity  - 삼성 디바이스의 기본 애니메이션과 충돌하여 생기는 Issue에 대한 솔루션

--- app/src/main/java/com/dining/coach/base/BaseActivity.kt
+++ app/src/main/java/com/dining/coach/base/BaseActivity.kt
@@ -1,7 +1,56 @@
 package com.dining.coach.base
 
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dining.coach.util.view.GridLayoutManagerWrapper
+import com.dining.coach.util.view.LinearLayoutManagerWrapper
 
-open class BaseActivity: AppCompatActivity() {
-    
+open class BaseActivity: AppCompatActivity(), OnClickListener {
+
+    fun setOnClickListeners(vararg views: View) {
+        try {
+            views.forEach { view ->
+                view.setOnClickListener(this)
+            }
+        }catch (e: Exception) {
+            // TODO : 상호작용 오류. 재 Inflate 요망.
+            // ERROR()
+        }
+    }
+
+    /**
+     * @suppress Inconsistency detected, Invalid view holder adapter positionViewHolder
+     * @param list = vararg variable of RecyclerView
+     * @author 강범석
+     * @see
+     * 삼성 Device에서 발생하는 Issue
+     * LayoutManager 사용 시, 에러 발생
+     * supportsPredictiveItemAnimations()와 삼성 핸드폰 애니메이션이 충돌하여 발생하는 현상
+     * 해당 상황 솔루션으로 Method 제공합니다.
+     */
+    protected fun wrapHorizontalRecyclerView(vararg list: RecyclerView) {
+        list.forEach { view ->
+            val manager = LinearLayoutManagerWrapper(this, LinearLayoutManager.HORIZONTAL, false)
+            view.layoutManager = manager
+        }
+    }
+
+    protected fun wrapVerticalRecyclerView(vararg list: RecyclerView) {
+        list.forEach { view ->
+            val manager = LinearLayoutManagerWrapper(this, LinearLayoutManager.VERTICAL, false)
+            view.layoutManager = manager
+        }
+    }
+
+    protected fun wrapGridRecyclerView(list: RecyclerView, spanCount: Int) {
+        val manager = GridLayoutManagerWrapper(this, spanCount)
+        list.layoutManager = manager
+    }
+
+    override fun onClick(p0: View?) {
+
+    }
 }
--- app/src/main/java/com/dining/coach/repo/user/UserRepositoryImpl.kt
+++ app/src/main/java/com/dining/coach/repo/user/UserRepositoryImpl.kt
@@ -1,9 +1,32 @@
 package com.dining.coach.repo.user
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
 
 ) : UserRepository {
+    fun test() {
+        val mainJob = CoroutineScope(Dispatchers.Default).launch {
+            val subJob: Job = launch(start = CoroutineStart.LAZY) {
+                for (idx in 0 until 20) {
+                    CoroutineScope(Dispatchers.IO).launch {
+
+                    }
+                }
+            }.job
+
+            launch {
+
+            }
+        }
+
+        mainJob.cancel()
+    }
 
 }
