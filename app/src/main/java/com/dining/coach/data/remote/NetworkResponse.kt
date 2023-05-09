a6171e75cc54b15248d8ac4cdc4c4b9c77acf02a
master <82108@DESKTOP-SDBJJB4>
2023-05-10 00:22:46 +0900

    [2023.05.10.강범석] 1. Retrofit Module Sample Guide

--- app/src/main/java/com/dining/coach/data/remote/NetworkResponse.kt
+++ app/src/main/java/com/dining/coach/data/remote/NetworkResponse.kt
@@ -1,13 +1,26 @@
 package com.dining.coach.data.remote
 
+import com.google.gson.JsonObject
 import java.io.IOException
 
-sealed class NetworkResponse<out T : Any> {
-    data class Success<T : Any>(val body: T?) : NetworkResponse<T>()
+sealed class NetworkResponse<out T : Any>(
+    open val apiInfo: APIInfo,
+    open val body: T? = null
+) {
+    data class Success<T : Any>(override val apiInfo: APIInfo, override val body: T?) : NetworkResponse<T>(apiInfo, body)
 
-    data class Failure<T: Any>(val code: Int, val error: String?) : NetworkResponse<T>()
+    data class Failure<T: Any>(override val apiInfo: APIInfo, val code: Int, val error: String?) : NetworkResponse<T>(apiInfo = apiInfo)
 
-    data class NetworkError(val error: IOException) : NetworkResponse<Nothing>()
+    data class NetworkError(override val apiInfo: APIInfo, val error: IOException) : NetworkResponse<Nothing>(apiInfo)
 
-    data class Unexpected(val t: Throwable) : NetworkResponse<Nothing>()
+    data class Unexpected(override val apiInfo: APIInfo, val t: Throwable) : NetworkResponse<Nothing>(apiInfo)
+
+    data  class returnError(override val apiInfo: APIInfo): NetworkResponse<Nothing>(apiInfo)
+    data class ResultError<T : Any>(override val apiInfo: APIInfo, val errorBody: T?): NetworkResponse<T>(apiInfo)
 }
+data class APIInfo(
+    val api: Int,
+    val jsonObject: JsonObject?,
+    val string: String?,
+    val uri: String? = null
+)
--- app/src/main/java/com/dining/coach/di/module/NetworkModule.kt
+++ app/src/main/java/com/dining/coach/di/module/NetworkModule.kt
@@ -1,4 +1,13 @@
 package com.dining.coach.di.module
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
+
 }
