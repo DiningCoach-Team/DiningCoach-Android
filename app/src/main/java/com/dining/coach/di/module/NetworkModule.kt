635821dfda9b94c39eea5250afc4ea79835ec51c
tunameatball <kh9768@gmail.com>
2023-05-13 15:50:55 +0900

    [2023.05.13.김광희] 1. Retrofit Client를 Hilt로 이동

--- app/src/main/java/com/dining/coach/di/module/NetworkModule.kt
+++ app/src/main/java/com/dining/coach/di/module/NetworkModule.kt
@@ -1,13 +1,38 @@
 package com.dining.coach.di.module
 
+import com.dining.coach.di.manager.remote.NetworkCallAdapter
+import com.dining.coach.util.const.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
 
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(NetworkCallAdapter.Factory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
 }
