17220ac6bdf80b21807181a2745df825d80d7c7f
WorldOneTop <dlwpdlf147@naver.com>
2023-06-01 11:39:28 +0900

    [2023.06.01.이제일] 1. 갤러리 화면 추가 및 로컬 이미지 데이터 가져오기 기능 추가  - 퍼미션 획득 실패 대응과 비동기 작업 변경 필요

--- app/src/main/java/com/dining/coach/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/dining/coach/ui/gallery/GalleryActivity.kt
@@ -1,15 +1,61 @@
 package com.dining.coach.ui.gallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.dining.coach.R
 import com.dining.coach.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class GalleryActivity : BaseActivity() {
+    private val viewModel: GalleryViewModel by viewModels()
+
+    private val requiredPermissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val requestPermissionsLauncher by lazy {
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { isGranted ->
+            if (isGranted.all { it.value }) {
+                // TODO("replace coroutine scope")
+                CoroutineScope(Dispatchers.IO).launch {
+                    viewModel.fetchImages(this@GalleryActivity)
+                }
+            } else {
+                // TODO("replace permission denied process")
+                Toast.makeText(this, "permission error", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
+        processGetGalleryData()
     }
+
+    private fun processGetGalleryData(){
+        if (hasAllPermissions()) {
+            // TODO("replace coroutine scope")
+            CoroutineScope(Dispatchers.IO).launch {
+                viewModel.fetchImages(this@GalleryActivity)
+            }
+        } else {
+            requestPermissionsLauncher.launch(requiredPermissions)
+        }
+    }
+
+    private fun hasAllPermissions() = requiredPermissions.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
 }
--- app/src/main/java/com/dining/coach/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/dining/coach/ui/gallery/GalleryViewModel.kt
@@ -1,12 +1,59 @@
 package com.dining.coach.ui.gallery
 
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import com.dining.coach.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
 
 ): BaseViewModel() {
+    private val INDEX_ID = MediaStore.MediaColumns._ID
+    private val INDEX_URI = MediaStore.MediaColumns.DATA
+    private val INDEX_DATE = MediaStore.MediaColumns.DATE_ADDED
 
+    private val _images = MutableStateFlow<List<Uri>>(emptyList())
+    val images: StateFlow<List<Uri>>
+        get() = _images
+
+    suspend fun fetchImages(context: Context) = withContext(Dispatchers.IO){
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            INDEX_ID,
+            INDEX_URI,
+            INDEX_DATE
+        )
+        val selection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.SIZE + " > 0"
+            else null
+        val sortOrder = "$INDEX_DATE DESC"
+        val cursor = context.contentResolver.query(
+            uri,
+            projection,
+            selection,
+            null,
+            sortOrder
+        )
+        cursor?.let {
+            val result = mutableListOf<Uri>()
+            val columnIndexID = cursor.getColumnIndexOrThrow(INDEX_ID)
+            var imageId: Long
+            while(cursor.moveToNext()) {
+                imageId = cursor.getLong(columnIndexID)
+                result.add(
+                    Uri.withAppendedPath(uri, imageId.toString())
+                )
+            }
+            _images.emit(result)
+        }
+        cursor?.close()
+    }
 }
